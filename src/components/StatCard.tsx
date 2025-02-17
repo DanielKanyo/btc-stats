da2820253f70f85ab7ae1941235de6b371a6e114
DanielKanyo <danielkanyog@gmail.com>
2025-02-17 15:24:22 +0100

    Css fix

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -8,10 +8,10 @@ interface StatCardPops {
 function StatCard({ label, value }: StatCardPops) {
     return (
         <Card shadow="xl" padding="xl" radius="lg">
-            <Text fz="sm" tt="uppercase" fw={600} lh={1} mb={14}>
+            <Text fz="sm" tt="uppercase" fw={600} lh={1} mb={12} c="gray.6">
                 {label}
             </Text>
-            <Text fz={45} fw={500} lh={1}>
+            <Text fz={45} fw={500} lh={1} c="gray.2">
                 {value}
             </Text>
         </Card>
